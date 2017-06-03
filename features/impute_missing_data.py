2a16b9e89a8bac29bde8c3d4416cba4ee6c7cffb
SunnyMarkLiu <SunnyMarkLiu101@gmail.com>
2017-06-03 16:29:49 +0800

    哭瞎，再也找不到 Kmeans 填充缺失的数据了

--- features/impute_missing_data.py
+++ features/impute_missing_data.py
@@ -43,9 +43,6 @@ def simple_impute_data(train, test):
     simple_filling_missing_data(train, ['state'], -1)
     simple_filling_missing_data(test, ['state'], -1)
 
-    simple_filling_missing_data(train, ['material'], 0)
-    simple_filling_missing_data(test, ['material'], 0)
-
     train['build_year'][train['build_year'] == 20052009] = 2005
     train['build_year'][train['build_year'] == 20] = 2000
     train['build_year'][train['build_year'] == 215] = 2015
@@ -58,17 +55,26 @@ def simple_impute_data(train, test):
     test['build_year'][test['build_year'] == 4965] = 1965
     test['build_year'][test['build_year'] == 71] = 1971
 
-    train['build_year'][train['build_year'] < 20] = None
-    test['build_year'][test['build_year'] < 20] = None
+    # train['build_year'][train['build_year'] < 20] = None
+    # test['build_year'][test['build_year'] < 20] = None
 
     return train, test
 
 
-def kmeans_impute_data(data_df, num_columns, missing_rates):
-    kmeans_impute_data = data_df[num_columns].copy()
-    total_count = data_df.shape[0]
+def kmeans_impute_datas(conbined_data, num_columns, missing_rates):
+
+    df_numeric = conbined_data[num_columns].copy()
+    # 对 str_columns 类别进行编码
+    df_obj = conbined_data.select_dtypes(include=['object']).copy()
+
+    for c in df_obj:
+        df_obj[c] = pd.factorize(df_obj[c])[0]
+
+    df_data = pd.concat([df_numeric, df_obj], axis=1)
+
+    total_count = conbined_data.shape[0]
     for missing_rate in missing_rates:
-        missing_df = data_df[num_columns].isnull().sum(axis=0).reset_index()
+        missing_df = conbined_data[num_columns].isnull().sum(axis=0).reset_index()
         missing_df.columns = ['column_name', 'missing_count']
         missing_df = missing_df[missing_df.missing_count > 0]
 
@@ -82,20 +88,20 @@ def kmeans_impute_data(data_df, num_columns, missing_rates):
 
         # print '填充缺失率大于{}的数据, 缺失数据属性 {}...'.format(missing_rate, missing_df.shape[0])
         # n_clusters 为超参数！
-        impute_model = KMeansImputeMissingData(data_df[num_columns], n_clusters=20, max_iter=100)
+        impute_model = KMeansImputeMissingData(conbined_data[num_columns], n_clusters=20, max_iter=100)
         kmeans_labels, x_kmeans, centroids, global_labels, x_global_mean = impute_model.impute_missing_data()
 
-        kmeans_impute_data[num_columns] = x_kmeans
+        df_data[num_columns] = x_kmeans
         # 填充数据
         big_missing_columns = missing_df[missing_df.missing_rate > missing_rate]['column_name']
-        data_df[big_missing_columns] = kmeans_impute_data[big_missing_columns]
+        conbined_data[big_missing_columns] = df_data[big_missing_columns]
 
-        missing_df = data_df[num_columns].isnull().sum(axis=0).reset_index()
+        missing_df = conbined_data[num_columns].isnull().sum(axis=0).reset_index()
         missing_df.columns = ['column_name', 'missing_count']
         missing_df = missing_df[missing_df.missing_count > 0]
         # print '缺失数据属性 {}'.format(missing_df.shape[0])
 
-    return data_df
+    return conbined_data
 
 
 def kmeans_impute_train_test_data(train, test):
@@ -109,9 +115,11 @@ def kmeans_impute_train_test_data(train, test):
     num_columns.remove('id')
     num_columns.remove('material')
     num_columns.remove('state')
+    # build_year 字段缺失过多，且 train test 分布基本一致，考虑进行默认填充
+    # num_columns.remove('build_year')
 
     missing_rates = [0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0]
-    conbined_data = kmeans_impute_data(conbined_data, num_columns, missing_rates)
+    conbined_data = kmeans_impute_datas(conbined_data, num_columns, missing_rates)
 
     train = conbined_data.iloc[:train.shape[0], :]
     train['price_doc'] = train_price_doc
@@ -157,7 +165,7 @@ def impute_macro_data(macro):
 
     num_columns = macro.select_dtypes(exclude=['object']).columns.values.tolist()
     missing_rates = [0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0]
-    macro = kmeans_impute_data(macro, num_columns, missing_rates)
+    macro = kmeans_impute_datas(macro, num_columns, missing_rates)
 
     return macro
 
@@ -170,8 +178,8 @@ def main():
     if contains_null(train) | contains_null(test) | contains_null(macro):
         print("填充 train, test 缺失数据...")
         train, test = simple_impute_data(train, test)
-        train, test = kmeans_impute_train_test_data(train, test)
         train, test = impute_categories_data(train, test)
+        train, test = kmeans_impute_train_test_data(train, test)
         print("填充 macro 缺失数据...")
         macro = impute_macro_data(macro)
         print("缺失数据填充完成")

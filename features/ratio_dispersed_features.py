2f66b07c34e2fe012e35f97a306a611cedde9865
SunnyMarkLiu <SunnyMarkLiu101@gmail.com>
2017-06-13 15:50:09 +0800

    ratio_dispersed_features LB Score 0.31768

--- features/ratio_dispersed_features.py
+++ features/ratio_dispersed_features.py
@@ -2,7 +2,7 @@
 # _*_ coding: utf-8 _*_
 
 """
-将生成的 ratio 等特征进行离散化分段处理
+将连续型特征进行离散化分段处理
 
 @author: MarkLiu
 @time  : 17-5-28 下午1:38
@@ -13,6 +13,7 @@ import sys
 module_path = os.path.abspath(os.path.join('..'))
 sys.path.append(module_path)
 
+import numpy as np
 import pandas as pd
 # remove warnings
 import warnings
@@ -25,7 +26,12 @@ import data_utils
 
 def dispersed_neighbourhood_ratio_features(conbined_data):
     """ 对 generate_neighbourhood_features 生成的特征进行离散化分段处理，同时删除原有过拟合的的 ratio 特征"""
+    features = ['life_sq_ratio', 'kitch_sq_ratio', 'kitch_sq_vs_life_ratio', '0_6_all_age_ratio', '7_14_all_age_ratio',
+                '0_17_all_age_ratio', '16_29_all_age_ratio', '0_13_all_age_ratio']
 
+    for f in features:
+        mingap = (conbined_data.life_sq.max() - conbined_data.life_sq.min()) / 10.0
+        conbined_data[f] = np.round(conbined_data[f].values / np.maximum(mingap,1.0))
     return conbined_data
 
 def main():
--- features/run.py
+++ features/run.py
@@ -22,8 +22,8 @@ os.system(cmd)
 cmd = 'python train_test_preprocess.py'
 os.system(cmd)
 
-# cmd = 'python generate_neighbourhood_features.py'
-# os.system(cmd)
+cmd = 'python generate_neighbourhood_features.py'
+os.system(cmd)
 
 cmd = 'python generate_longitude_latitude_features.py'
 os.system(cmd)

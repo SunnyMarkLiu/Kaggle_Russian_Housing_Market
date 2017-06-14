5667844a645a0853cdae29e30f3a6be4b9852dc1
SunnyMarkLiu <SunnyMarkLiu101@gmail.com>
2017-06-14 14:33:57 +0800

    delete deal_multicollinearity, try PCA

--- features/run.py
+++ features/run.py
@@ -31,9 +31,6 @@ os.system(cmd)
 # cmd = 'python ratio_dispersed_features.py'
 # os.system(cmd)
 
-# cmd = 'python deal_multicollinearity.py'
-# os.system(cmd)
-
 # cmd = 'python generate_time_window_features.py'
 # os.system(cmd)
 

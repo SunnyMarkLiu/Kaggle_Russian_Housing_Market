01502f77efa92826d8b2b611e3d341d98ba45a87
SunnyMarkLiu <SunnyMarkLiu101@gmail.com>
2017-06-15 15:03:57 +0800

    优化 impute_missing_data.py

--- features/impute_missing_data.py
+++ features/impute_missing_data.py
@@ -17,7 +17,6 @@ from collections import Counter
 
 # my own module
 import data_utils
-from kmeans_impute_missing_data import KMeansImputeMissingData
 # remove warnings
 import warnings
 
@@ -140,72 +139,6 @@ def simple_impute_data_preprocess(train, test):
     return train, test
 
 
-def kmeans_impute_datas(conbined_data, num_columns, missing_rates):
-
-    df_numeric = conbined_data[num_columns].copy()
-    # 对 str_columns 类别进行编码
-    df_obj = conbined_data.select_dtypes(include=['object']).copy()
-
-    for c in df_obj:
-        df_obj[c] = pd.factorize(df_obj[c])[0]
-
-    df_data = pd.concat([df_numeric, df_obj], axis=1)
-
-    total_count = conbined_data.shape[0]
-    for missing_rate in missing_rates:
-        missing_df = conbined_data[num_columns].isnull().sum(axis=0).reset_index()
-        missing_df.columns = ['column_name', 'missing_count']
-        missing_df = missing_df[missing_df.missing_count > 0]
-
-        if missing_df.shape[0] == 0:  # 不存在缺失数据
-            break
-
-        missing_df = missing_df.sort_values(by='missing_count', ascending=False)
-        missing_df['missing_rate'] = 1.0 * missing_df['missing_count'] / total_count
-        if missing_df['missing_rate'].values[0] < missing_rate:
-            continue
-
-        # print '填充缺失率大于{}的数据, 缺失数据属性 {}...'.format(missing_rate, missing_df.shape[0])
-        # n_clusters 为超参数！
-        impute_model = KMeansImputeMissingData(conbined_data[num_columns], n_clusters=20, max_iter=100)
-        kmeans_labels, x_kmeans, centroids, global_labels, x_global_mean = impute_model.impute_missing_data()
-
-        df_data[num_columns] = x_kmeans
-        # 填充数据
-        big_missing_columns = missing_df[missing_df.missing_rate > missing_rate]['column_name']
-        conbined_data[big_missing_columns] = df_data[big_missing_columns]
-
-        missing_df = conbined_data[num_columns].isnull().sum(axis=0).reset_index()
-        missing_df.columns = ['column_name', 'missing_count']
-        missing_df = missing_df[missing_df.missing_count > 0]
-        # print '缺失数据属性 {}'.format(missing_df.shape[0])
-
-    return conbined_data
-
-
-def kmeans_impute_train_test_data(train, test):
-    conbined_data = pd.concat([train[test.columns.values], test])
-    conbined_data.columns = test.columns.values
-    train_price_doc = train['price_doc']
-
-    num_columns = conbined_data.select_dtypes(exclude=['object']).columns.values.tolist()
-
-    # 去除类别属性的数值类型的 column
-    num_columns.remove('id')
-    num_columns.remove('material')
-    num_columns.remove('state')
-    # build_year 字段缺失过多，且 train test 分布基本一致，考虑进行默认填充
-    # num_columns.remove('build_year')
-
-    missing_rates = [0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0]
-    conbined_data = kmeans_impute_datas(conbined_data, num_columns, missing_rates)
-
-    train = conbined_data.iloc[:train.shape[0], :]
-    train['price_doc'] = train_price_doc
-    test = conbined_data.iloc[train.shape[0]:, :]
-    return train, test
-
-
 def impute_categories_data(train, test):
     str_columns = train.select_dtypes(include=['object']).columns.values.tolist()
     impute_categories_missing_data(train, str_columns)
@@ -242,11 +175,62 @@ def impute_macro_data(macro):
     macro['old_education_build_share'][macro['old_education_build_share'] == '25,47'] = 2547
     macro['old_education_build_share'][macro['old_education_build_share'] == '8,2517'] = 82517
 
-    num_columns = macro.select_dtypes(exclude=['object']).columns.values.tolist()
-    missing_rates = [0.6, 0.5, 0.4, 0.3, 0.2, 0.1, 0]
-    macro = kmeans_impute_datas(macro, num_columns, missing_rates)
+    # 此处简单处理
+    macro.fillna(-1, inplace=True)
 
     return macro
+
+
+def impute_missing_data_based_on_distribution(train, test):
+    """
+    ◇ 特征值为连续值：按不同的分布类型对缺失值进⾏补全：偏正态分布，使⽤均值代替，可以保持数
+                   据的均值；偏长尾分布，使⽤中值代替，避免受 outlier 的影响；
+    ◇ 特征值为离散值：使⽤众数代替。
+    """
+    train_id = train['id']
+    train_price_doc = train['price_doc']
+    train.drop(['id', 'price_doc'], axis=1, inplace=True)
+    test_id = test['id']
+    test.drop(['id'], axis=1, inplace=True)
+
+    # 合并训练集和测试集
+    conbined_data = pd.concat([train[test.columns.values], test])
+    conbined_data.columns = test.columns.values
+
+    # 特征值为连续值：按不同的分布类型对缺失值进⾏补全：偏正态分布，使⽤均值代替，可以保持数
+    #              据的均值；偏长尾分布，使⽤中值代替，避免受 outlier 的影响；
+    mean_features = ['kitch_sq', 'hospital_beds_raion', 'cafe_avg_price_500', 'cafe_sum_500_max_price_avg',
+                     'cafe_sum_500_min_price_avg', 'build_year', 'max_floor', 'life_sq', 'preschool_quota',
+                     'school_quota', 'cafe_avg_price_1000', 'cafe_sum_1000_max_price_avg', 'build_count_block',
+                     'cafe_sum_1000_min_price_avg', 'build_count_1971-1995', 'build_count_1946-1970',
+                     'build_count_after_1995', 'build_count_before_1920', 'build_count_1921-1945',
+                     'raion_build_count_with_builddate_info', 'build_count_panel', 'build_count_monolith',
+                     'raion_build_count_with_material_info', 'build_count_brick', 'cafe_avg_price_1500',
+                     'cafe_sum_1500_max_price_avg', 'cafe_sum_1500_min_price_avg', 'cafe_sum_2000_min_price_avg',
+                     'cafe_avg_price_2000', 'cafe_sum_2000_max_price_avg', 'cafe_sum_3000_min_price_avg',
+                     'cafe_sum_3000_max_price_avg', 'cafe_avg_price_3000', 'cafe_sum_5000_max_price_avg',
+                     'cafe_sum_5000_min_price_avg', 'cafe_avg_price_5000', 'prom_part_5000', 'floor',
+                     'ID_railroad_station_walk', 'railroad_station_walk_min', 'railroad_station_walk_km',
+                     'metro_km_walk', 'metro_min_walk', 'green_part_2000', 'full_sq']
+    for mf in mean_features:
+        mean = np.expm1(np.log1p(conbined_data[mf].dropna()).mean())
+        simple_filling_missing_data(conbined_data, [mf], mean)
+
+    median_features = ['num_room', 'build_count_foam', 'build_count_slag', 'build_count_mix', 'build_count_frame',
+                       'build_count_wood']
+    # 特征值为离散值：使⽤众数代替
+    for mf in median_features:
+        median = np.expm1(np.log1p(conbined_data[mf].dropna()).median())
+        simple_filling_missing_data(conbined_data, [mf], median)
+
+    train = conbined_data.iloc[:train.shape[0], :]
+    test = conbined_data.iloc[train.shape[0]:, :]
+
+    train['id'] = train_id
+    train['price_doc'] = train_price_doc
+    test['id'] = test_id
+
+    return train, test
 
 
 def main():
@@ -258,7 +242,7 @@ def main():
         print("填充 train, test 缺失数据...")
         train, test = simple_impute_data_preprocess(train, test)
         train, test = impute_categories_data(train, test)
-        train, test = kmeans_impute_train_test_data(train, test)
+        train, test = impute_missing_data_based_on_distribution(train, test)
         print("填充 macro 缺失数据...")
         macro = impute_macro_data(macro)
         print("缺失数据填充完成")

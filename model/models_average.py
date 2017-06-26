33d52e8bad9122bc61da946e4fe1950e10939f04
SunnyMarkLiu <SunnyMarkLiu101@gmail.com>
2017-06-26 11:27:34 +0800

    10 single best model average, LB Score: 0.30942

--- model/models_average.py
+++ model/models_average.py
@@ -5,14 +5,12 @@
 @author: MarkLiu
 @time  : 17-6-26 上午11:00
 """
-from __future__ import absolute_import, division, print_function
-
 import os
 import sys
 
 module_path = os.path.abspath(os.path.join('..'))
 sys.path.append(module_path)
-
+import numpy as np
 import pandas as pd
 from conf.configure import Configure
 
@@ -27,11 +25,14 @@ xgboost_model_031328 = pd.read_csv('./my_best_result/xgboost_model_0.31328.csv')
 xgboost_model_031344 = pd.read_csv('./my_best_result/xgboost_model_0.31344.csv')
 xgboost_model_031392 = pd.read_csv('./my_best_result/xgboost_model_0.31392.csv')
 
-average_result = 0.30 * (0.784 * xgboost_model_031051['price_doc'] + 0.216 * xgboost_model_031191['price_doc']) + \
+average_result1 = 0.30 * (0.784 * xgboost_model_031051['price_doc'] + 0.216 * xgboost_model_031191['price_doc']) + \
                  0.25 * (0.784 * xgboost_model_031211['price_doc'] + 0.216 * xgboost_model_031284['price_doc']) + \
                  0.20 * (0.784 * xgboost_model_031289['price_doc'] + 0.216 * xgboost_model_031295['price_doc']) + \
                  0.15 * (0.784 * xgboost_model_031323['price_doc'] + 0.216 * xgboost_model_031328['price_doc']) + \
                  0.10 * (0.784 * xgboost_model_031344['price_doc'] + 0.216 * xgboost_model_031392['price_doc'])
+
+# average_result1 0.30957
+average_result = 0.784 * average_result1 + 0.216 * xgboost_model_031051['price_doc']  # 0.30942
 
 df_sub = pd.DataFrame({'id': xgboost_model_031051['id'], 'price_doc': average_result})
 df_sub.to_csv(Configure.submission_path, index=False)

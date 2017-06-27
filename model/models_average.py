7d763b0ec7f1f13e61cfc630b73ccda29aaa2f8c
SunnyMarkLiu <SunnyMarkLiu101@gmail.com>
2017-06-27 16:42:55 +0800

    8 single best model average, LB Score: 0.30935

--- model/models_average.py
+++ model/models_average.py
@@ -10,7 +10,7 @@ import sys
 
 module_path = os.path.abspath(os.path.join('..'))
 sys.path.append(module_path)
-import numpy as np
+
 import pandas as pd
 from conf.configure import Configure
 
@@ -22,17 +22,14 @@ xgboost_model_031289 = pd.read_csv('./my_best_result/xgboost_model_0.31289.csv')
 xgboost_model_031295 = pd.read_csv('./my_best_result/xgboost_model_0.31295.csv')
 xgboost_model_031323 = pd.read_csv('./my_best_result/xgboost_model_0.31323.csv')
 xgboost_model_031328 = pd.read_csv('./my_best_result/xgboost_model_0.31328.csv')
-xgboost_model_031344 = pd.read_csv('./my_best_result/xgboost_model_0.31344.csv')
-xgboost_model_031392 = pd.read_csv('./my_best_result/xgboost_model_0.31392.csv')
 
-average_result1 = 0.30 * (0.784 * xgboost_model_031051['price_doc'] + 0.216 * xgboost_model_031191['price_doc']) + \
-                 0.25 * (0.784 * xgboost_model_031211['price_doc'] + 0.216 * xgboost_model_031284['price_doc']) + \
-                 0.20 * (0.784 * xgboost_model_031289['price_doc'] + 0.216 * xgboost_model_031295['price_doc']) + \
-                 0.15 * (0.784 * xgboost_model_031323['price_doc'] + 0.216 * xgboost_model_031328['price_doc']) + \
-                 0.10 * (0.784 * xgboost_model_031344['price_doc'] + 0.216 * xgboost_model_031392['price_doc'])
+average_result1 = 0.35 * (0.784 * xgboost_model_031051['price_doc'] + 0.216 * xgboost_model_031191['price_doc']) + \
+                  0.30 * (0.784 * xgboost_model_031211['price_doc'] + 0.216 * xgboost_model_031284['price_doc']) + \
+                  0.20 * (0.784 * xgboost_model_031289['price_doc'] + 0.216 * xgboost_model_031295['price_doc']) + \
+                  0.15 * (0.784 * xgboost_model_031323['price_doc'] + 0.216 * xgboost_model_031328['price_doc'])
 
-# average_result1 0.30957
-average_result = 0.784 * average_result1 + 0.216 * xgboost_model_031051['price_doc']  # 0.30942
+# average_result1
+average_result = 0.784 * average_result1 + 0.216 * xgboost_model_031051['price_doc']  # 0.30935
 
 df_sub = pd.DataFrame({'id': xgboost_model_031051['id'], 'price_doc': average_result})
 df_sub.to_csv(Configure.submission_path, index=False)
